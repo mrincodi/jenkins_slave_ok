193c67bc5328a35553613ace8374deae19074765
Mario R. Rincón-Díaz (Mario R. Rincón) <mrincodi@gmail.com>
2016-09-25 13:04:49 -0700

    Changes to make it Rubocop-compliant

--- recipes/create_user.rb
+++ recipes/create_user.rb
@@ -6,26 +6,27 @@
 
 # Create user.
 user node['username'] do
-	# This 'supports' one is needed to create the folder.
-	supports :manage_home => true
-	action :create
-	group node['groupname']
-	home  node['homedir']
-	shell node['shell']
+  # This 'supports' one is needed to create the folder.
+  # supports :manage_home => true
+  supports manage_home: true
+  action :create
+  group node['groupname']
+  home  node['homedir']
+  shell node['shell']
 end
 
 # Create .ssh and give permissions.
 # TODO: Constants?
 directory '/home/worker/.ssh' do
-	owner node['username']
-	group node['groupname']
-	mode '0700'
+  owner node['username']
+  group node['groupname']
+  mode '0700'
 end
 
 # Create authorized_keys and give permissions.
 template '/home/worker/.ssh/authorized_keys' do
-	source 'authorized_keys.erb'
-	owner node['username']
-	group node['groupname']
-	mode '0600'
+  source 'authorized_keys.erb'
+  owner node['username']
+  group node['groupname']
+  mode '0600'
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,3 @@ include_recipe 'java_se::default'
 include_recipe 'jenkins_slave_ok::junit'
 
 include_recipe 'jenkins_slave_ok::create_user'
-
-#include_recipe 'jenkins_slave_ok::git'
-
